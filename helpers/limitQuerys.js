05171fc608de6689b5ca7d2529105b2313bb00b3
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-16 14:15:29 -0500

    feat: :sparkles: Creamos la configuracion para limitar el tamaño del json y la cantidad de querys que se pueden solicitar

---
+++ helpers/limitQuerys.js
@@ -0,0 +1,24 @@
+import rateLimit from 'express-rate-limit';
+export let queryCliente = () => {
+    return rateLimit({
+        windowMs: 30 * 1000,
+        max: 10,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req, res) => {
+            if(req.headers["content-length"]>=250) {
+                res.status(413).send({
+                    status: 413,
+                    message: "El objeto que esta mandando supera el limite de tamaño, cambielo"
+                });
+                return true;
+            }
+        },
+        message: (req, res) => {
+            res.status(429).send({
+                status: 429,
+                message: "Pailas menor, no puede solicitar tanto >:C",
+            })
+        },
+    })
+}
