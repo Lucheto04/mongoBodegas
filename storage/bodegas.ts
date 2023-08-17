89ee4b8507f00710b7874a0f1a0bbbe09e0b30a7
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-17 16:46:27 -0500

    refactor: :ambulance: Corregimos el DTO de Bodega

--- storage/bodegas.ts
+++ storage/bodegas.ts
@@ -1,38 +1,47 @@
 import { Expose, Transform } from 'class-transformer';
-import { IsDefined } from 'class-validator';
-
+import { IsDefined} from 'class-validator';
 export class Bodega {
 
-    @Expose({ name: 'id' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro id es obligatorio y debe ser un numero entero (int).`}}})
+    @Expose({ name: '_id' })
+    @Transform(({ value }) => { if(value) return value ; else 0})
     _id: number;
 
     @Expose({ name: 'Nombre-Bodega' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro Nombre-Bodega es obligatorio y debe ser un string` }}})
+    @IsDefined({message: ()=>{ throw {status: 422, message: `El Nombre-Bodega es obligatorio y debe ser un string.`}}})
     nombre: string;
 
     @Expose({ name: 'Responsable-id' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro Responsable-id es obligatorio y debe ser un numero entero (int).`}}})
+    @IsDefined({message: ()=>{ throw {status: 422, message: `El Responsable-id es obligatorio y debe ser un numero entero (int).`}}})
     id_responsable: number;
 
     @Expose({ name: 'Estado-Bodega' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro Estado-Bodega es obligatorio y debe ser un numero entero (int).`}}})
+    @IsDefined({message: ()=>{ throw {status: 422, message: `El Estado-Bodega es obligatorio y debe ser un numero entero (int).`}}})
     estado: number;
 
-    @Expose({ name: 'Creado-Por' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro Creado-Por es obligatorio y debe ser un numero entero (int).`}}})
+    @Expose({ name: 'created_by' })
+    @Transform(({ value }) => { if(value) return value ; else 0})
     created_by: number;
 
-    @Expose({ name: 'Creado-Hora' })
-    @IsDefined({ message: () => { throw { status: 422, message: `El parametro Creado-Hora es obligatorio y debe ser un string` }}})
+    @Expose({ name: 'update_by' })
+    @Transform(({ value }) => { if(value) return value ; else 0})
+    update_by: number;
+
+    @Expose({ name: 'created_at' })
+    @Transform(({ value }) => { if(value) return value ; else "faker"})
     created_at: string;
 
-    constructor (data: Partial<Bodega>) {
-        this._id = 0;
+    @Expose({ name: 'updated_at' })
+    @Transform(({ value }) => { if(value) return value ; else "faker"})
+    updated_at: string;
+
+    @Expose({ name: 'deleted_at' })
+    @Transform(({ value }) => { if(value) return value ; else "faker"})
+    deleted_at: string;
+
+    constructor(data:Partial<Bodega>) {
+        Object.assign(this, data);
         this.nombre = "Faker";
         this.id_responsable = 0;
         this.estado = 0;
-        this.created_by = 0;
-        this.created_at = "YYYY-MM-DD"
     }
 }
