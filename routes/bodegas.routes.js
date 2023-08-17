e44dde24b79af94cd8529e2677ce41cee4b599c5
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-17 16:21:03 -0500

    feat: :sparkles: Creamos la funcion de ayuda siguienteId
    Esta funcion es para implementar el autoincrement en mongo

--- routes/bodegas.routes.js
+++ routes/bodegas.routes.js
@@ -2,6 +2,7 @@ import { coneccion } from "../db/atlas.js";
 import { limitQuery } from "../helpers/limitQuerys.js";
 import { appMiddlewareBodegaVerify, appDTOBodega } from "../middleware/middle.bodegas.js";
 import { Router } from "express";
+import siguienteId from "../helpers/siguienteId.js";
 let appBodega = Router();
 
 let db = await coneccion();
@@ -12,4 +13,35 @@ appBodega.get('/', limitQuery(), appMiddlewareBodegaVerify, async(req, res) => {
     let result = await bodegas.find().sort({ nombre: 1 }).toArray();
     res.send(result)
 })
+
+
+
+// let {cc:CC, nombre:NOMBRE} = json;
+// let json2 = {};
+// Object.assign(json2, {CC, NOMBRE});
+
+/**
+* ! POST
+* ? agregar una nueva bodega
+* * http://127.0.0.3:5012/bodega
+*/
+appBodega.post('/', limitQuery(), appMiddlewareBodegaVerify, appDTOBodega, async(req, res) => {
+    if (!req.rateLimit) return;
+    try {
+        const newId = await siguienteId( "bodegas");
+
+        
+        const newDocument = {
+            _id: newId,
+            ...req.body
+        };
+
+        let result = await bodegas.insertOne(newDocument);
+        console.log(result);
+        res.status(201).send({ status: 201, message: 'documento creado con exito' });
+    } catch (error) {
+        console.log(error);
+        res.status(406).send('no se ha podido crear el documento');
+    }
+});
 export default appBodega;
